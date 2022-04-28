efeab6ac84cbc907a41ff9251783ee68b90a87f9
zara <zara.afridi15@gmail.com>
2022-04-28 03:20:41 -0400

    Added head to layout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import useScrollDirection from '../../hooks/useScrollDirection';
-import Social from '../Social/social';
 import Footer from '../Footer/footer';
+import Head from '../Head/head';
 import './layout.css';
 
 const Layout = ({children}) => {
@@ -36,31 +36,36 @@ const Layout = ({children}) => {
     }
     return '';
   }; 
-  scrollDirection == 'up' ? console.log('HELL YEAH BROTHER') : console.log('down');
-  const ariaHidden = navState() === 'nav-down' ? 'false' : 'true';
+  scrollDirection === 'up' ? console.log('HELL YEAH BROTHER') : console.log('down');
   console.log('navState', navState());
 
   return (
-    <div className='flex flex-col justify-center mx-8'>
-      <nav className='nav-container flex flex-row items-center justify-end p-8' data-nav-state={navState()}>
-        <a role='button' href="#main" className='skip-nav'>
-          Skip To Content
-        </a>
-        <a role='button' href='#about' className='mr-4'>
-          About
-        </a>
-        <a role='button' href='#work' className='mr-4'>
-          Work
-        </a>
-        <a role='button' href='#projects' className='mr-32'>
-          Projects
-        </a>
-      </nav>
-      <main id='main' className='p-2 md:p-8'>
-        {children}
-      </main>
-      <Footer></Footer>
-    </div>
+    <>
+    <Head></Head>
+      <div className='flex flex-col justify-center mx-8'>
+        <nav className='nav-container flex flex-row items-center justify-end p-8' data-nav-state={navState()}>
+          <a role='button' href="#main" className='skip-nav'>
+            Skip To Content
+          </a>
+          <a role='button' href='#about' className='mr-8 p-2'>
+            About
+          </a>
+          <a role='button' href='#work' className='mr-8 p-2'>
+            Work
+          </a>
+          <a role='button' href='#projects' className='mr-8 p-2'>
+            Projects
+          </a>
+          <a href='/resume.pdf' className='mr-32 border-dotted border-2 focus:outline-0 focus:border-pink focus:text-pink hover:border-pink hover:text-pink p-2 rounded'>
+            Resume
+          </a>
+        </nav>
+        <main id='main' className='p-2 md:p-8'>
+          {children}
+        </main>
+        <Footer></Footer>
+      </div>
+    </>
   )
 };
 
