53a01d8e578bf425d2d0b0e7637096ac9e7ba859
zara <zara.afridi15@gmail.com>
2022-04-27 21:40:25 -0400

    Removed commented template

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,22 +15,7 @@ const IndexPage = () => {
           <h1 id='title' className='text-5xl font-extrabold text-center'>Hi! I'm <span className='highlight'>Zara Afridi</span></h1>
           <h2 className='text-2xl pt-4 text-center'>Software Developer @ <span className='font-bold'>Publicis Sapient</span></h2>
         </div>
-        {/* <div className='h-fit shadow transform hover:scale-[1.03] transition-all rounded-full w-full md:w-1/3 2xl:w-1/4 bg-gradient-to-r from-[#D1B5DF] to-[#C77890] p-2'>
-          <div className='bg-black p-4 rounded-full'>
-            <StaticImage
-              alt="A headshot of a woman with dark brown hair and a white sweater smiling into the camera."
-              src="../images/final_headshot.jpg"
-              className='rounded-full'
-            />
-          </div>
-        </div> */}
       </div>
-      {/* <div className='my-8' id="about">
-        <div className='text-5xl font-extrabold py-8'>About</div>
-        <div className='flex flex-col'>
-          <p></p>
-        </div>
-      </div> */}
       <div>
         <About></About>
       </div>
