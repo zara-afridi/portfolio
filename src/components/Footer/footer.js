9f73e3fc9dc829bf881657de150b6c7cafc3bf0a
zara <zara.afridi15@gmail.com>
2022-04-28 03:20:30 -0400

    Added link styling

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
       <Social></Social>
       <div className='text-center pb-8'>
-        <a href='https://github.com/afridiz1/portfolio' target='_blank' className='highlight transition duration-200 border-b border-transparent hover:border-pink'>
+        <a href='https://github.com/afridiz1/portfolio' target='_blank' rel='noreferrer' className='highlight transition duration-200 border-b border-transparent hover:border-pink'>
           Built & Designed By Zara Afridi
         </a>
       </div>
