e859107e5e3a0c69f0f431ab5cba9c46c11a145f
zara <zara.afridi15@gmail.com>
2022-04-27 22:18:30 -0400

    Updated scroll top padding

--- src/components/TileGroup/tileGroup.js
+++ src/components/TileGroup/tileGroup.js
@@ -11,14 +11,14 @@ const TileGroup = () => {
   console.log('jobData',jobData);
   return (
     <div>
-      <div id={jobData.id} className='my-8'>
-        <div className='text-5xl font-extrabold py-8'>{jobData.title}</div>
+      <div id={jobData.id} className='mb-8 pt-8 scroll-mt-8'>
+        <div className='text-5xl font-extrabold pb-8 pt-4'>{jobData.title}</div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           {jobData.tiles.map(tile => (<Tile key={tile.id}>{tile}</Tile>))}
         </div>
       </div>
-      <div id={projectData.id} className='my-8'>
-        <div className='text-5xl font-extrabold py-8'>{projectData.title}</div>
+      <div id={projectData.id} className='mb-8 pt-8 scroll-mt-8'>
+        <div className='text-5xl font-extrabold pb-8 pt-4'>{projectData.title}</div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           {projectData.tiles.map(tile => (<Tile key={tile.id}>{tile}</Tile>))}
         </div>
