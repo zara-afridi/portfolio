5fb67858a1614da1d9162b89e1a5c6c22a12adf2
zara <zara.afridi15@gmail.com>
2022-04-29 17:06:38 -0400

    updated scroll top margin

--- src/components/TileGroup/tileGroup.js
+++ src/components/TileGroup/tileGroup.js
@@ -9,13 +9,13 @@ const TileGroup = () => {
   
   return (
     <div>
-      <div id={jobData.id} className='mb-8 pt-8 scroll-mt-8'>
+      <div id={jobData.id} className='mb-8 pt-8 scroll-mt-4'>
         <div className='text-5xl font-extrabold pb-8 pt-4'>{jobData.title}</div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           {jobData.tiles.map(tile => (<Tile key={tile.id}>{tile}</Tile>))}
         </div>
       </div>
-      <div id={projectData.id} className='mb-8 pt-8 scroll-mt-8'>
+      <div id={projectData.id} className='mb-8 pt-8 scroll-mt-4'>
         <div className='text-5xl font-extrabold pb-8 pt-4'>{projectData.title}</div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           {projectData.tiles.map(tile => (<Tile key={tile.id}>{tile}</Tile>))}
